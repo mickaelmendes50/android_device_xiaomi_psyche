6eafbe256e817b1e19b9a1b0993c6bb463d3759c
Alexander Koskovich <zvnexus@outlook.com>
2020-11-27 13:55:32 -0700

    psyche: Build Paranoid Doze.
     * Supports stuff like pulsing notifications on pickup.
    
    Change-Id: Idfe70fb3b3f66677c0b3ab9c6d19aec045ca41a0
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -161,6 +161,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpmhalservice.enable=1
 
+# Doze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sensor.pickup=xiaomi.sensor.pickup
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
