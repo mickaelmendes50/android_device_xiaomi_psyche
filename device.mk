9a02745d662edf05decf3b92c7565f2da2e619f9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-17 15:51:33 +0200

    psyche: Configure refresh rate settings
    Change-Id: I757580052fd1e84208d26311b088674b0f31482c
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -160,6 +160,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.use_layer_ext=0 \
     vendor.display.use_smooth_motion=0
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.set_idle_timer_ms=1500 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.defer_fps_frame_count=2
+
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpmhalservice.enable=1
@@ -253,6 +262,7 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     AOSPAPsycheFrameworks \
     PsycheFrameworks \
+    PsycheSettings \
     PsycheSystemUI \
     FrameworksResTarget \
     WifiResTarget
