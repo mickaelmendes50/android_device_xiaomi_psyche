ea23d05143fb09762e3a54d6821d25426f769b4e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-23 17:53:10 +0530

    psycho: Import WCNSS configuration from LA.UM.9.12.r1-13500.01-SMxx50.QSSI12.0
    Change-Id: I4c503f828554b677b6a6df8d3c4573a958c9c8b3
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -165,3 +165,7 @@ $(call inherit-product, vendor/xiaomi/psyche/psyche-vendor.mk)
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
+# WLAN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg_qca6390.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
