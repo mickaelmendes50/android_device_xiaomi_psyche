620c1311024197be09ba3d782a646a91dac78988
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-09 01:34:54 +0200

    psyche: Implement UDFPS handler
    Change-Id: I7afff305140639d38ebb8bc51fbe790a1636db9e
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -187,7 +187,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor \
+    libudfpshandler
 
 # FRP
 PRODUCT_VENDOR_PROPERTIES += \
