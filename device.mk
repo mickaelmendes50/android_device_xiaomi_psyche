ec7e8b0e66765390dde3ce38188cee7dbf80dcd3
Dyneteve <dyneteve@hentaios.com>
2022-05-31 23:12:19 +0900

    psyche: Enable SSR for all subsytem types
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -301,6 +301,10 @@ PRODUCT_PACKAGES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Subsystem silent restart
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.ssr.restart_level=ALL_ENABLE
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
