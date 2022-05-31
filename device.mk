1eefe65f7d33a96ce9a251a73e6ad10c858ac964
Park Ju Hyung <qkrwngud825@gmail.com>
2022-05-31 18:32:42 +0900

    psyche: relax timers for automatic refresh rate switching
    Due to visible gamma shifts on our panels, we should make the switchings
    to happen conservatively.
    
    With idle timer >= touch timer, idle timer now always takes precedence,
    but we're leaving touch timer property to immediately switch back to
    90 Hz on touch input events.
    
    Display power timer is left untouched as it's responsible for display
    mode changes such as Doze.
    
    Change-Id: If8b1e00a2d2213c4d2288ab28fad4ba0826a36e8
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -155,8 +155,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.use_smooth_motion=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=1500 \
-    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
