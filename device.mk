ea931b35ba155c3c55319bd0feaf14c0a027d2ef
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-07-30 13:25:56 +0000

    psyche: Add QCRIL properties from MIUI
    Change-Id: Ia674f1db40616b9079012d40582dc537b67d175c
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -245,6 +245,14 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Platform
 TARGET_BOARD_PLATFORM := kona
 
+# QCRIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.cdma_cap=true \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.force_ltd_sys_ind=1 \
+    persist.vendor.radio.manual_nw_rej_ct=1
+
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := all
 
