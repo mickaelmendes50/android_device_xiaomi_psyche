3081b7bfe0599c2ad87336266cf9b82d587c7c66
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-07 05:25:56 +0000

    psyche: Remove common bt and telephony HIDL interfaces
    After commit: https://gerrit.aospa.co/c/AOSPA/android_device_qcom_common/+/22567
    These bt and telephony interfaces are built common for all qcom devices which are
    including bt and telephony components in their tree.
    Therefore, eliminate these packages from this device tree.
    
    Change-Id: Ib5c65019654fe1e63028d430b4745cf08bea73b3
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -311,16 +311,10 @@ PRODUCT_COPY_FILES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
     android.hardware.drm@1.4.vendor \
     android.hardware.gatekeeper@1.0.vendor \
     android.hardware.keymaster@4.1.vendor \
     android.hardware.neuralnetworks@1.3.vendor \
-    android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.radio@1.5.vendor \
-    android.hardware.secure_element@1.2.vendor \
-    android.system.net.netd@1.1.vendor
 
 # WLAN
 PRODUCT_COPY_FILES += \
