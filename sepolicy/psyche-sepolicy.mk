92cc13866093ebb32984503ee02e35c66cbb695f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-19 10:45:39 +0000

    psyche: sepolicy: Rewrite device specific rules from scratch
    Fixes broken rules, excessive permissions and unneeded or obsolete rules
    Also restucture rules with sepolicy macros.
    
    Change-Id: Iecc3c487a6402c4df7e9078eaa0ec2cb607ccc02
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- sepolicy/psyche-sepolicy.mk
+++ sepolicy/psyche-sepolicy.mk
@@ -24,9 +24,7 @@ BOARD_SEPOLICY_DIRS += \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/camera \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/fingerprint \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/ir \
-    device/xiaomi/psyche/sepolicy/vendor/xiaomi/last_kmsg \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/power_supply \
-    device/xiaomi/psyche/sepolicy/vendor/xiaomi/radio \
     device/xiaomi/alioth/sepolicy/vendor/xiaomi/sensors \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/thermald \
     device/xiaomi/psyche/sepolicy/vendor/xiaomi/touch \
