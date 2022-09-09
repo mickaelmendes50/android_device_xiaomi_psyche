c77fe3e8931a55b5d26dad9fd7684e05c8677885
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-09 15:49:40 +0200

    psyche: udfps: Update handler methods
    Change-Id: I7778ba69dcdf2843d3c11f5bd0ec92f1887f7395

--- configs/udfps/UdfpsHandler.cpp
+++ configs/udfps/UdfpsHandler.cpp
@@ -100,6 +100,14 @@ class XiaomiKonaUdfpsHander : public UdfpsHandler {
         int arg[2] = {TOUCH_FOD_ENABLE, FOD_STATUS_OFF};
         ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
     }
+
+    void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
+        // nothing
+    }
+
+    void cancel() {
+        // nothing
+    }
   private:
     fingerprint_device_t *mDevice;
     android::base::unique_fd touch_fd_;
