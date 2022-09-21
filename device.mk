c2a2ef7742e18883a0a062c2fff22e6936760f09
Omkar Chandorkar <gotenksIN@aospa.co>
2022-09-21 11:56:02 +0900

    psyche: enable smooth motion in surfaceflinger
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Change-Id: I1567fde6213b8b60d38019a4f1c510819f264159

--- device.mk
+++ device.mk
@@ -164,9 +164,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
-PRODUCT_ODM_PROPERTIES += \
-    vendor.display.use_smooth_motion=0
-
 # DPM
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.dpmhalservice.enable=1
