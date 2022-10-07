f9b0680153556b2cce901115915510a7acb4756c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-07 01:06:13 -0300

    psyche: init: Add custom init to override properties per variant
    [mickaelmendes50]
    - adapt to psyche
    
    Change-Id: Ia1a6e4af8160c2a8d937f7a618f8b38dbd3cd78e
    Co-authored-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Co-authored-by: Mesquita <mesquita@aospa.co>

---
+++ init/init_psyche.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <vector>
+
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
+
+using android::base::GetProperty;
+
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info* pi;
+
+    pi = (prop_info*)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_dalvikvm_properties() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+    if(sys.totalram > 6144ull * 1024 * 1024) {
+    // from - phone-xhdpi-8192-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heaptargetutilization", "0.46");
+        property_override("dalvik.vm.heapmaxfree", "48m");
+    }
+}
+
+void vendor_load_properties() {
+    property_override("ro.product.brand", "Xiaomi");
+    property_override("ro.product.device", "psyche");
+    property_override("ro.product.manufacturer", "Xiaomi");
+    property_override("ro.product.model", "2112123AG");
+    property_override("ro.product.name", "psyche");
+
+    load_dalvikvm_properties();
+}
