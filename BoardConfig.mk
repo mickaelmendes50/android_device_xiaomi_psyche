ebb9ff4eb1a090fe721a41ab4192564f5550ee3a
Jake Weinstein <jake@aospa.co>
2023-02-18 02:17:48 +0900

    psyche: Use vendor-common compatibility matrix
    Already called in device/qcom/common
    
    Change-Id: Id30aa7e4827556cbcc1deae9471d6f43a22fa33a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,9 +92,6 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_manifest.xml
 
-DEVICE_MATRIX_FILE += \
-    device/qcom/common/compatibility_matrix.xml
-
 # Hacks
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
