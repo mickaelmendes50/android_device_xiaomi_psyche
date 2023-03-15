f72ec3edd4e5d9776970b0ab94d347f04c93729b
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-03-14 23:34:20 -0300

    psyche: overlay: Update multiple vibration intensity levels config for T
    Since commit: https://android.googlesource.com/platform/packages/apps/Settings.git/+/eaaf5331d64ab54ce55a857fc73a186d5f256fc5
    multiple intensities configuration flag has been changed to an interger
    reflect that change in here to allow multiple intensity vibration pattern in settings.
    
    Change-Id: I13f9dbf6280623ad1c954ce96ceecbe363140062
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -231,6 +231,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Overlays
 PRODUCT_PACKAGES += \
     AOSPAPsycheFrameworks \
+    AOSPAPsycheSettings \
     AOSPAPsycheSystemUI \
     PsycheCarrierConfig \
     PsycheFrameworks \
