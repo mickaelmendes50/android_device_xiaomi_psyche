a258a3f62c1306d60284ff84636ffd6fd33df8f2
Mesquita <mesquita@aospa.co>
2023-03-15 08:54:19 -0300

    psyche: enable keymaster component
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -265,6 +265,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     dsprpcd \
     gps \
     init \
+    keymaster \
     media \
     nfc \
     overlay \
@@ -344,9 +345,7 @@ PRODUCT_COPY_FILES += \
 # VNDK
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.gatekeeper@1.0.vendor \
-    android.hardware.keymaster@4.1.vendor \
-    android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.neuralnetworks@1.3.vendor
 
 # WLAN
 PRODUCT_COPY_FILES += \
@@ -355,4 +354,3 @@ PRODUCT_COPY_FILES += \
 # WFD
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
-
