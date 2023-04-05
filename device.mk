f0b7ccc9bb0fcdfb3e91272ef8b5b5edf0f4e0b0
Mesquita <mesquita@aospa.co>
2023-04-05 11:12:51 -0300

    [DNM] psyche: disable tws plus feature
     * still broken after QPR2 merge, so keep it disable
    
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.vendor.btstack.enable.lpa=true \
-    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.enable.twsplus=false \
     persist.vendor.bt.a2dp.aac_whitelist=false
 
 # Boot
