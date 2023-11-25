d1328c80169ea05c293ce0bf41847d0cd1a817c8
Mesquita <mesquita@aospa.co>
2023-11-25 10:31:05 -0300

    psyche: move ```vndservicemanager``` to qcom/common
    Change-Id: If5a81e109f2cec87a41718b4f2621ef7349b5ade
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -322,10 +322,6 @@ PRODUCT_PACKAGES += \
     update_engine_sideload \
     update_verifier
 
-# Vendor Service Manager
-PRODUCT_PACKAGES += \
-    vndservicemanager
-
 # Vendor
 $(call inherit-product, vendor/xiaomi/psyche/psyche-vendor.mk)
 
